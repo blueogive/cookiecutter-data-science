aadbb8e181685ae13098fca0366168e543c40401
Mark Coggeshall <mark.coggeshall@gmail.com>
2017-01-02 14:40:47 -0500

    Example Python script illustrating use of pyncrypt, dotenv.

--- {{ cookiecutter.repo_name }}/src/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/src/data/make_dataset.py
@@ -1,19 +1,52 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+"""Clean raw data from (../raw) into processed data (../processed)."""
+
+# This import_parents function is required to permit the pyncrypt import
+# work since the pyncrypt module is in a parent directory one level up
+# and this module is not part of formal Python package.
+import importlib
+import sys
+from pathlib import Path
+
+
+def import_parents(level=1):
+    """Boilerplate code for setting __package__ attribute for relative imports.
+
+    Supplement for http://stackoverflow.com/a/28154841/2301450
+    """
+    global __package__
+    file = Path(__file__).resolve()
+    parent, top = file.parent, file.parents[level]
+    __package__ = '.'.join(parent.parts[len(top.parts):])
+    sys.path.append(str(top))
+    importlib.import_module(__package__)  # won't be needed after that
+
+
+if __name__ == '__main__' and __package__ is None:
+    import_parents(level=1)
+
+
+from pyncrypt import KeyStore
+import dotenv
 import os
 import click
 import logging
-from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
+def dostuff(input_filepath, output_filepath):
+    """Dummy function."""
+    pass
+
+def main():
+    """The main function."""
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+    logger.info("mike's password is {pwd}".format(pwd=VLT.require('mike')))
 
 
 if __name__ == '__main__':
@@ -25,6 +58,9 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+    dotenv.load_dotenv(dotenv.find_dotenv())
+
+    VLT = KeyStore(os.environ['PASSPHRASE'])  # PASSPHRASE is defined in .env
+    VLT.require('mike')
 
     main()
